402735cf54aab3ca18f5c6a79f86c286525d21a0
Jukyung <sungjukyung94@gmail.com>
2025-06-10 21:30:58 +0900

    rix eslint error

--- src/app/(overall)/layout.tsx
+++ src/app/(overall)/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import "../index.css";
 import Menus from "@/components/nav-links";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Jukyung Dev",
