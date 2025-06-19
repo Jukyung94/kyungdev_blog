6e226680ccb37d81ec80ca0ef9b940451e072f21
Jukyung <sungjukyung94@gmail.com>
2025-06-19 15:47:25 +0900

    fixed type error

--- src/app/(overall)/dev/[id]/page.tsx
+++ src/app/(overall)/dev/[id]/page.tsx
@@ -1,9 +1,9 @@
 import ReadForm from "@/components/read-form";
 
-export default async function Page(props: any) {
+export default async function Page(props: { params: { id: string }}) {
   const params = await props.params;
   const id = params.id;
   return (
-    <ReadForm id={id}  />
+    <ReadForm id={id} />
   )
 }
--- src/app/(overall)/logs/[id]/page.tsx
+++ src/app/(overall)/logs/[id]/page.tsx
@@ -1,6 +1,6 @@
 import ReadForm from "@/components/read-form";
 
-export default async function Page(props: any) {
+export default async function Page(props: { params: { id: string }}) {
   const params = await props.params;
   const id = params.id;
   return (
