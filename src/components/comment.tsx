fc94d1fa65a3fd8aafdcf60d548bda1530cf5da2
Jukyung <sungjukyung94@gmail.com>
2025-06-27 02:46:39 +0900

    change comment form style

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -18,26 +18,33 @@ export default function Comment(props: { id: string }) {
   }, [])
 
   return(
-      <div className="col">
-        <span>댓글</span>
-        {prevComments?.map((item, idx) => (
-          <div key={idx}>
-            <div>{item.author}</div>
-            <div>{item.content}</div>
-          </div>
-        ))}
-        <div>
-          <div>
+      <div className="col gap">
+        <span>Comments</span>
+        <hr />
+        <div className="prev_comment">
+          {prevComments?.map((item, idx) => (
+            <div key={idx}>
+              <div>{item.author}</div>
+              <div>{item.content}</div>
+            </div>
+          ))}
+        </div>
+        <div className="comment">
+          <div className="name">
             <label htmlFor="author">name</label>
             <input type="text" name="author" onChange={(event) => {
               setComment({...comment, author: event.target.value})
             }}/>
           </div>
+          <textarea name="comment" id="" placeholder="Comment" value={comment.content} onChange={(event) => {
+            setComment({...comment, content: event.target.value})
+          }} />
+          <div className="row wing">
+            <button>
+              <span>SUBMIT</span>
+            </button>
+          </div>
         </div>
-        <input name="comment" id="" placeholder="댓글내용" value={comment.content} onChange={(event) => {
-          setComment({...comment, content: event.target.value})
-        }} />
-        <button>등록</button>
       </div>
   )
 }
